e626745fa965db058abea58e0ba8fc0841f2c6f9
alireza <moosaviafzal@gmail.com>
2024-04-10 22:38:51 +0330

    init Func

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import "fmt"
 
-func PowerH(base, power int) int {
-
-	sum := 1
-	for i := 0; i < power; i++ {
-		sum *= base + PowerH(base, 0-power)
-
-	}
-	return sum
+//func PowerH(base, power int) int {
+//
+//	sum := 1
+//	for i := 0; i < power; i++ {
+//		sum *= base + PowerH(base, 0-power)
+//
+//	}
+//	return sum
+//}
+//
+//func main() {
+//
+//	fmt.Println(PowerH(2, 5))
+func init() {
+	fmt.Println("hello from init func")
 }
 
 func main() {
 
-	fmt.Println(PowerH(2, 5))
+	fmt.Println("hello from main func")
 }
-
-//interface
-
-//type assertion
--- main_test.go
+++ main_test.go
@@ -1,40 +1,41 @@
 package main
 
-import "testing"
-
-func TestPowerH(t *testing.T) {
-	type test struct {
-		Base           int
-		Power          int
-		ExpectedResult int
-	}
-
-	var testCases = []test{
-		{Base: 1, Power: 2, ExpectedResult: 1},
-		{Base: 0, Power: 2, ExpectedResult: 0},
-		{Base: 10, Power: 2, ExpectedResult: 100},
-		{Base: 1, Power: 200, ExpectedResult: 1},
-		{Base: 1, Power: 0, ExpectedResult: 1},
-		{Base: 2, Power: 1, ExpectedResult: 2},
-		{Base: 400, Power: 2, ExpectedResult: 160000},
-		{Base: 0, Power: 0, ExpectedResult: 1},
-	}
-
-	for _, c := range testCases {
-		res := PowerH(c.Base, c.Power)
-
-		if res != c.ExpectedResult {
-			t.Errorf("expecte: %d , result: %d", c.ExpectedResult, res)
-		}
-	}
-
-}
-
-var result int
-
-func BenchmarkPowerH(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		res := PowerH(2, 1)
-		result = res
-	}
-}
+//
+//import "testing"
+//
+//func TestPowerH(t *testing.T) {
+//	type test struct {
+//		Base           int
+//		Power          int
+//		ExpectedResult int
+//	}
+//
+//	var testCases = []test{
+//		{Base: 1, Power: 2, ExpectedResult: 1},
+//		{Base: 0, Power: 2, ExpectedResult: 0},
+//		{Base: 10, Power: 2, ExpectedResult: 100},
+//		{Base: 1, Power: 200, ExpectedResult: 1},
+//		{Base: 1, Power: 0, ExpectedResult: 1},
+//		{Base: 2, Power: 1, ExpectedResult: 2},
+//		{Base: 400, Power: 2, ExpectedResult: 160000},
+//		{Base: 0, Power: 0, ExpectedResult: 1},
+//	}
+//
+//	for _, c := range testCases {
+//		res := PowerH(c.Base, c.Power)
+//
+//		if res != c.ExpectedResult {
+//			t.Errorf("expecte: %d , result: %d", c.ExpectedResult, res)
+//		}
+//	}
+//
+//}
+//
+//var result int
+//
+//func BenchmarkPowerH(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		res := PowerH(2, 1)
+//		result = res
+//	}
+//}
