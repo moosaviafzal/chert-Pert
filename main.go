14c7e7b21386e5fed402885ec9eba12e6b076538
alireza <moosaviafzal@gmail.com>
2024-04-11 23:59:03 +0330

    initi func

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import "fmt"
 
-//func PowerH(base, power int) int {
+// func PowerH(base, power int) int {
+// func PowerH(base, power int) int {
 //
-//	sum := 1
-//	for i := 0; i < power; i++ {
-//		sum *= base + PowerH(base, 0-power)
+//		sum := 1
+//		for i := 0; i < power; i++ {
+//			sum *= base + PowerH(base, 0-power)
 //
+//		}
+//		return sum
 //	}
-//	return sum
-//}
 //
-//func main() {
+// func main() {
 //
 //	fmt.Println(PowerH(2, 5))
 func init() {
-	fmt.Println("hello from init func")
+	fmt.Prfunc`infunctln("hello from init func")
+	fmtg
 }
 
 func main() {
